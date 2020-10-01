9a3e933989a795c37b0b240acdf50cec4a34b69f
Cedric Negouai <c.negouai@gmail.com>
2020-10-01 16:34:50 +0200

    Sidebar2

---
+++ sidebar/sidebar.php
@@ -0,0 +1,27 @@
+
+    
+    <!-- début Sidebar -->
+		<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar" class="active">
+                <h1><a href="index.html" class="logo" ><img src="../sidebar/images/philiance_favicon.png"/></a></h1>
+                <ul class="list-unstyled components mb-4">
+                    <li class="active">
+                    <a href="#"><span class="fa fa-home"></span> Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="fa fa-building"></span> Salles</a>
+                    </li>
+                    <li>
+                    <a href="#"><span class="fa fa-list"></span> Liste</a>
+                    </li>
+                    <li>
+                    <a href="#"><span class="fa fa-sign-out"></span> Logout</a>
+                    </li>
+                </ul>
+                <button type="button" id="sidebarCollapse" class="btn btn-primary">
+                    <i class="fa fa-arrows-h"></i>
+                    <span class="sr-only"></span>
+                </button>
+         </nav>
+		</div>
+     
